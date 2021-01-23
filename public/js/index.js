e607f1293c83ed272ca31956079305095db86a0a
Sarthak Kavidayal <setu.kavidayal@gmail.com>
2021-01-24 00:21:21 +0530

    responsive nav bar non-working drop down menue

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,18 @@
 const socketHandler = (data, userdata) => {
     const socket = io()
+
+    const userInfo = document.getElementById('user-info')
+    userInfo.innerHTML = `<h1>Hi,<img src='image/avatar.jpg' alt='avatar' class='user-image'></h1>
+                            <h1>${userdata.username}</h1>`
     
-    const contactsList = document.getElementById('contacts-list')
-    let str = `<h1>CONTACTS</h1>` 
+    const contactsList = document.getElementById('people-list')
+    let str = `<h2>PEOPLE</h2>` 
 
     data.forEach(user => {
         if(user === userdata.username) {
           return
         }
-        str = str + `<div class="list"><a href="#">${user}</a></div>`  
+        str = str + `<div class="list">${user}<button class="add-button">Add</button></div>`  
     })
 
     contactsList.innerHTML = str
@@ -63,8 +67,37 @@ const checkForUser = async () => {
 checkForUser()
 
 
+addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        document.getElementsByClassName('send-button')[0].click()
+    }
+})
+
 document.getElementById('side-bar-toggle').addEventListener('click', () => {
     const sideBar = document.getElementsByClassName('side-bar')[0]
-    console.log(sideBar)
-    console.log(sideBar.style)
+    const css = sideBar.currentStyle || window.getComputedStyle(sideBar)
+
+    if(window.screen.availWidth < 1024) {
+        sideBar.style.width = "150%"
+    } else {
+        sideBar.style.width = "25%"
+    }
+
+    if(css.marginLeft === "0px") {
+        if(window.screen.availWidth < 1024) {
+            sideBar.style.marginLeft = "-150%"
+        } else {
+            sideBar.style.marginLeft = "-25%"
+        }
+    } else {
+        sideBar.style.marginLeft = "0px"
+    }
+
+    console.log(window.screen.availHeight, window.screen.availWidth)
+})
+
+document.getElementById('user-circle-toggle-button').addEventListener('click', () => {
+    console.log("worked")
+    const dropDownMenue = document.getElementsByClassName('drop-down-menue')[0]
+    dropDownMenue.style.display = "block" 
 })
